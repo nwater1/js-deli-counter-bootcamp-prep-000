1589e4e57e1176d17de82249439b1512826a386f
Jatupon Jantri <jameball007@gmail.com>
2018-02-04 17:58:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -28,21 +28,18 @@ function nowServing(katzDeli) {
 
 function currentLine(katzDeli) {
   if (katzDeli.length > 0) {
-    var list = []
+    
     var word = "The line is currently:"
     for (var i = 0; i < katzDeli.length; i++ ) {
-      var name = `${i+1}. ${katzDeli[i]}`
+      word += `${i+1}. ${katzDeli[i]}`
       if (katzDeli.length - i > 1) {
         
         var comma = "," 
-        var full = name + comma
-        list.push(full)
-      } else {
-        var full = sub
-        list.push(full)
-      }
+        word += comma
+        
+      } 
      }
-    return word + list[]
+    return word 
   } else {
     return "The line is currently empty."
   }
